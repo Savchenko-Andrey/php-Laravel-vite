776dd6f8bd2f9c1697d62d3d5d3d8c03f7aa1469
Savchenko-Andrey <dracxm95@gmail.com>
2025-07-15 09:08:28 +0200

    refactor: remove tailwindcss import from vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import laravel from 'laravel-vite-plugin'
 
 export default defineConfig({
   plugins: [
@@ -8,6 +7,12 @@ export default defineConfig({
       input: ['resources/css/app.css', 'resources/js/app.js'],
       refresh: true,
     }),
-    tailwindcss(),
   ],
-});
+  build: {
+    manifest: true,
+    outDir: 'public/build',
+    rollupOptions: {
+      input: 'resources/js/app.js',
+    },
+  },
+})
